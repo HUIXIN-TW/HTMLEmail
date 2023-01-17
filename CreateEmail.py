427b78848fb37595d9049bcac43fc6ea8d3a761a
HUIXIN-TW <huixin.yang.tw@gmail.com>
2023-01-17 23:30:16 +0800

    add service details

--- CreateEmail.py
+++ CreateEmail.py
@@ -5,7 +5,7 @@ from Client import Client as C
 
 def main():
     ClientName = sys.argv[1]
-    PATH = f'Client/{ClientName}'
+    PATH = f'Client/{ClientName}.json'
     Client = C(PATH)
     with open('template.html') as f:
         content = f.read()
@@ -16,7 +16,7 @@ def main():
         content = content.replace('{CLEANER_CONTACT}', Client.CLEANER_CONTACT)
         content = content.replace('{LOCATION}', Client.LOCATION)
         content = content.replace('{SERVICE_TYPE}', Client.SERVICE_TYPE)
-        content = content.replace('{SERVICE_DETAIL}', Client.SERVICE_DETAIL)
+        content = content.replace('<!-- {SERVICE_DETAIL} -->', Client.SERVICE_DETAIL)
         content = content.replace('{SERVICE_PAYMENT}', Client.SERVICE_PAYMENT)
         content = content.replace('{SERVICE_NOTE}', Client.SERVICE_NOTE)
         content = content.replace('{SERVICE_DDMMHHmm}', Client.SERVICE_DDMMHHmm)
